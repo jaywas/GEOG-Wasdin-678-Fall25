84bbbf9112966f6273bc0faf8fff31fcb9bd26bb
Jay Wasdin <jaywas@tamu.edu>
2025-11-12 16:46:55 -0600

    Lab updates

--- lab/week04/Java lab.js
+++ lab/week04/Java lab.js
@@ -1,1 +1,36 @@
-function Run 
+function Run()
+{
+    var myObject =
+    {
+    	myValue: 'some text',
+        subObject:
+        {
+        	deeperValue1: 6,
+            deeperValue2: "more stuff"
+        }
+    };
+    
+    console.log(myObject);
+}
+
+function Run2()
+{
+    var person1 =  BuildObject('dan', 39, 4.0);
+    var person2 =  BuildObject('edgar', 25, 2.0);
+    var person3 =  BuildObject('paul', 55, 3.9);
+
+    console.log(person1);
+    console.log(person2);
+    console.log(person3);
+}
+function BuildObject(value1, value2, value3)
+{
+    var returnObject =
+    {
+        name: value1,
+        age: value2,
+        gpa: value3
+    }
+
+    return returnObject;
+}
