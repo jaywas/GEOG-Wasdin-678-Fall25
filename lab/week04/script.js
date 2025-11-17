1f3ae2d5f1b9555edf0b618009885109560e9902
Jay Wasdin <jaywas@tamu.edu>
2025-11-17 14:40:21 -0600

    updates

--- lab/week04/script.js
+++ lab/week04/script.js
@@ -34,16 +34,16 @@ function BuildObject(value1, value2, value3)
 
     return returnObject;
 }
-function Run3()
+function Run3() 
 {
-    var person1 =  BuildObject('dan', 39, 4.0);
-    var person2 =  BuildObject('edgar', 25, 2.0);
-    var person3 =  BuildObject('paul', 55, 3.9);
+    var person1 = BuildObject('dan', 39, 4.0);
+    var person2 = BuildObject('edgar', 25, 2.0);
+    var person3 = BuildObject('paul', 55, 3.9);
 
     var students = [person1, person2, person3];
 
-    var students = FindWorstStudent(students);
+    var worstStudent = FindWorstStudent(students);
 
-    console.log("The worst student has been found. It is: ");
+    console.log("The worst student has been found. It is:");
     console.log(worstStudent);
-}    
+}
