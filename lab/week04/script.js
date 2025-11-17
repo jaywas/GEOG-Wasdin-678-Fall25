d1a6d00503398696ef158f13ee1ea7a964726cd7
Jay Wasdin <jaywas@tamu.edu>
2025-11-17 16:16:27 -0600

    updates

--- lab/week04/script.js
+++ lab/week04/script.js
@@ -61,9 +61,6 @@ function FindWorstStudent(listofstudents)
     });
 
     return worstStudent;
-
-    console.log("The worst student has been found. It is:");
-    console.log(worstStudent);
 }
 
 function Run4()
@@ -75,4 +72,109 @@ function Run4()
 
     console.log("The worst student has been found. It is:");
     console.log(worstStudent);
+}
+
+function findTopLeft(pointlist) {
+    var maxLat = null;
+    var minLon = null;
+
+    pointlist.data.forEach(
+        point => {
+            if (maxLat == null) {
+                maxLat = point.lat
+                minLon = point.lon
+        }
+        else {
+            if (point.lat > maxLat){
+                maxLat = point.lat
+            }
+            if (point.lon < minLon){
+                minLon = point.lon
+            }
+        }
+    }
+    )
+    return {"pointId": 1, "lat": maxLat, "lon": minLon}
+}
+function findTopRight(pointlist) {
+    var maxLat = null;
+    var maxLon = null;
+
+    pointlist.data.forEach(
+        point => {
+            if (maxLat == null) {
+                maxLat = point.lat
+                maxLon = point.lon
+        }
+        else {
+            if (point.lat > maxLat){
+                maxLat = point.lat
+            }
+            if (point.lon > maxLon){
+                maxLon = point.lon
+            }
+        }
+    }
+    )
+    return {"pointId": 2, "lat": maxLat, "lon": maxLon}
+}
+function findBottomLeft(pointlist) {
+    var minLat = null;
+    var minLon = null;
+
+    pointlist.data.forEach(
+        point => {
+            if (minLat == null) {
+                minLat = point.lat
+                minLon = point.lon
+        }
+        else {
+            if (point.lat < minLat){
+                minLat = point.lat
+            }
+            if (point.lon < minLon){
+                minLon = point.lon
+            }
+        }
+    }
+    )
+    return {"pointId": 3, "lat": minLat, "lon": minLon}
+}
+function findBottomRight(pointlist) {
+    var minLat = null;
+    var maxLon = null;
+
+    pointlist.data.forEach(
+        point => {
+            if (minLat == null) {
+                minLat = point.lat
+                maxLon = point.lon
+        }
+        else {
+            if (point.lat < minLat){
+                minLat = point.lat
+            }
+            if (point.lon > maxLon){
+                maxLon = point.lon
+            }
+        }
+    }
+    )
+    return {"pointId": 4, "lat": minLat, "lon": maxLon}
+}
+function Run(){
+    var topLeftPoint = findTopLeft(theJSON)
+    var topRightPoint = findTopRight(theJSON)
+    var bottomLeftPoint = findBottomLeft(theJSON)
+    var bottomRightPoint = findBottomRight(theJSON)
+
+    var boundingBox = {
+        topLeft: topLeftPoint,
+        topRight: topRighttPoint,
+        bottomLeft: bottomLeftPoint,
+        bottomRight: bottomRightPoint,
+    }
+
+    console.log("Bounding box computed for Jaywas@tamu.edu: ");
+    console.log(boundingbox);
 }
